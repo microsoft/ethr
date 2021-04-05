a45e9313f2ac346f9364887bd6a61ed15cb30284
Jared Moore <jared.moore@getweave.com>
2021-04-05 16:21:05 -0600

    INCOMPLETE: Session Stub

--- session/session.go
+++ session/session.go
@@ -1,1 +1,12 @@
 package session
+
+import (
+	"weavelab.xyz/ethr/ethr"
+)
+
+
+func CreateAckMsg () (msg *ethr.Msg){
+	msg = &ethr.Msg{Version: 0, Type: ethr.Ack}
+	msg.Ack = &ethr.MsgAck{}
+	return
+}
