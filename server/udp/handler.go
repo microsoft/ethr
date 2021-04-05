4fd4a9428be5c39795bc888ff74548952ddc6cbd
Jared Moore <jared.moore@getweave.com>
2021-04-05 16:43:14 -0600

    UDP Server Breakout

---
+++ server/udp/handler.go
@@ -0,0 +1,82 @@
+package udp
+
+import (
+	"net"
+	"sync/atomic"
+	"time"
+	"weavelab.xyz/ethr/ethr"
+)
+
+type Handler struct {
+	session ethr.Session
+	logger ethr.Logger
+}
+
+
+func (h Handler) HandleConn(conn *net.UDPConn) {
+	// This local map aids in efficiency to look up a test based on client's IP
+	// address. We could use createOrGetTest but that takes a global lock.
+	// TODO move caching to session
+	tests := make(map[string]*ethr.Test)
+	// For UDP, allocate buffer that can accomodate largest UDP datagram.
+	readBuffer := make([]byte, 64*1024)
+	n, remoteIP, err := 0, new(net.UDPAddr), error(nil)
+
+	// This function handles UDP tests that came from clients that are no longer
+	// sending any traffic. This is poor man's garbage collection to ensure the
+	// server doesn't end up printing dormant client related statistics as UDP
+	// has no reliable way to detect if client is active or not.
+	// TODO move to session handling
+	go func() {
+		for {
+			time.Sleep(100 * time.Millisecond)
+			for k, v := range tests {
+				h.logger.Debug("Found Test from server: %v, time: %v", k, v.LastAccess)
+				// At 200ms of no activity, mark the test in-active so stats stop
+				// printing.
+				if time.Since(v.LastAccess) > (200 * time.Millisecond) {
+					v.IsDormant = true
+				}
+				// At 2s of no activity, delete the test by assuming that client
+				// has stopped.
+				if time.Since(v.LastAccess) > (2 * time.Second) {
+					h.logger.Debug("Deleting UDP test from server: %v, lastAccess: %v", k, v.LastAccess)
+					h.session.SafeDeleteTest(v)
+					delete(tests, k)
+				}
+			}
+		}
+	}()
+	for err == nil {
+		n, remoteIP, err = conn.ReadFromUDP(readBuffer)
+		if err != nil {
+			h.logger.Debug("Error receiving data from UDP for bandwidth test: %v", err)
+			continue
+		}
+		//ethrUnused(remoteIP)
+		//ethrUnused(n)
+		//server, port, _ := net.SplitHostPort(remoteIP.String())
+		server, _, _ := net.SplitHostPort(remoteIP.String())
+		test, found := tests[server]
+		if !found {
+			test, isNew := h.session.CreateOrGetTest(server, ethr.UDP, ethr.TestTypeAll)
+			if test != nil {
+				tests[server] = test
+			}
+			if isNew {
+				h.logger.Debug("Creating UDP test from server: %v, lastAccess: %v", server, time.Now())
+				// TODO handle externally
+				//ui.emitTestHdr()
+			}
+		}
+		if test != nil {
+			test.IsDormant = false
+			test.LastAccess = time.Now()
+			atomic.AddUint64(&test.Result.PPS, 1)
+			atomic.AddUint64(&test.Result.Bandwidth, uint64(n))
+		}
+		//else {
+			//h.logger.Debug("Unable to create test for UDP traffic on port %s from %s port %s", gEthrPortStr, server, port)
+		//}
+	}
+}
---
+++ server/udp/server.go
@@ -0,0 +1,38 @@
+package udp
+
+import (
+	"fmt"
+	"net"
+	"runtime"
+	"weavelab.xyz/ethr/ethr"
+	"weavelab.xyz/ethr/server"
+)
+
+type HandlerFunc func(net.Conn)
+
+func Serve(cfg *server.Config, h HandlerFunc) error {
+	udpAddr, err := net.ResolveUDPAddr(ethr.UDPVersion(cfg.IPVersion), cfg.LocalIP+":"+cfg.LocalPort)
+	if err != nil {
+		return fmt.Errorf("unable to resolve UDP address: %w", err)
+	}
+	l, err := net.ListenUDP(ethr.UDPVersion(cfg.IPVersion), udpAddr)
+	if err != nil {
+		return fmt.Errorf("error listening on %s for UDP pkt/s tests: %w", cfg.LocalPort, err)
+	}
+	// Set socket buffer to 4MB per CPU so we can queue 4MB per CPU in case Ethr is not
+	// able to keep up temporarily.
+	err = l.SetReadBuffer(runtime.NumCPU() * 4 * 1024 * 1024)
+	if err != nil {
+		return fmt.Errorf("failed to set ReadBuffer on UDP socket: %w", err)
+	}
+	//
+	// We use NumCPU here instead of NumThreads passed from client. The
+	// reason is that for UDP, there is no connection, so all packets come
+	// on same CPU, so it isn't clear if there are any benefits to running
+	// more threads than NumCPU(). TODO: Evaluate this in future.
+	//
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go h(l)
+	}
+	return nil
+}
